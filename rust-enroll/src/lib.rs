138b14400efb84ba488af8efd0dd9ab4669997c3
Mahhheshh <100200105+Mahhheshh@users.noreply.github.com>
2025-01-09 10:27:14 +0530

    add airdrop

--- rust-enroll/src/lib.rs
+++ rust-enroll/src/lib.rs
@@ -1,16 +1,19 @@
 #[cfg(test)]
 mod tests {
 
+    use solana_client::rpc_client::RpcClient;
     use solana_sdk;
     use solana_sdk::{
         pubkey::Pubkey,
-        signature::{Keypair, Signer},
+        signature::{read_keypair_file, Keypair, Signer},
     };
 
     // for pk conversion
     use bs58;
     use std::io::{self, BufRead};
 
+    // rpc url
+    const RPC_URL: &str = "https://api.devnet.solana.com";
     #[test]
     fn keygen() {
         // Create a new keypair and a new wallet with it
@@ -57,9 +60,23 @@ mod tests {
         println!("{:?}", base58);
     }
 
-    // air drop some tokens
+    // request airdrop test
     #[test]
-    fn airdop() {}
+    fn airdop() {
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        let client = RpcClient::new(RPC_URL);
+
+        match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+            Ok(s) => {
+                println!("Success! Check out your TX here:");
+                println!(
+                    "https://explorer.solana.com/tx/{}?cluster=devnet",
+                    s.to_string()
+                );
+            }
+            Err(e) => println!("Oops, something went wrong: {}", e.to_string()),
+        };
+    }
 
     #[test]
     fn transfer_sol() {}
