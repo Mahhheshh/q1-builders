fcf32d240b5e3d6cf6e9bbfea66f53d971a1644a
Mahhheshh <100200105+Mahhheshh@users.noreply.github.com>
2025-01-09 10:01:32 +0530

    add keygen test

--- rust-enroll/src/lib.rs
+++ rust-enroll/src/lib.rs
@@ -2,13 +2,28 @@
 mod tests {
 
     use solana_sdk;
-    
+    // import the required functions
+    use solana_sdk::{
+        pubkey::Pubkey,
+        signature::{Keypair, Signer},
+    };
+
     #[test]
-    fn keygen() {}
+    fn keygen() {
+        // Create a new keypair and a new wallet with it
+        let kp = Keypair::new();
+        println!(
+            "I've generated a new Solana wallet: {}", // Apcqkwih6xiuKTUzc51UZMeVGcbsdM1NCtPgavWRuYDp
+            kp.pubkey().to_string()
+        );
+        println!("");
+        println!("To save your wallet, copy and paste the following into a JSON file:");
+        println!("{:?}", kp.to_bytes());
+    }
 
     #[test]
     fn airdop() {}
-    
+
     #[test]
     fn transfer_sol() {}
 }
