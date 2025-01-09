075ee0770151a07f52b104d7e11ff6d459aab322
Mahhheshh <100200105+Mahhheshh@users.noreply.github.com>
2025-01-09 10:41:57 +0530

    test: add transfer 0.1 sol

--- rust-enroll/src/lib.rs
+++ rust-enroll/src/lib.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 mod tests {
-
     use solana_client::rpc_client::RpcClient;
     use solana_sdk;
     use solana_sdk::{
-        pubkey::Pubkey,
         signature::{read_keypair_file, Keypair, Signer},
+        transaction::Transaction,
     };
+    use std::str::FromStr;
 
     // for pk conversion
     use bs58;
@@ -14,6 +14,8 @@ mod tests {
 
     // rpc url
     const RPC_URL: &str = "https://api.devnet.solana.com";
+
+    use solana_program::{pubkey::Pubkey, system_instruction::transfer, system_program};
     #[test]
     fn keygen() {
         // Create a new keypair and a new wallet with it
@@ -79,5 +81,29 @@ mod tests {
     }
 
     #[test]
-    fn transfer_sol() {}
+    fn transfer_sol() {
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        let to_pubkey = Pubkey::from_str("BKYX34S6RJP4YjuLfcDRr2aBboCNL43ku4k4FuY9nTU1").unwrap();
+        // Create a Solana devnet connection
+        let rpc_client = RpcClient::new(RPC_URL);
+        // Get recent blockhash
+        let recent_blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
+        let transaction = Transaction::new_signed_with_payer(
+            &[transfer(&keypair.pubkey(), &to_pubkey, 100_000_000u64)],
+            Some(&keypair.pubkey()),
+            &vec![&keypair],
+            recent_blockhash,
+        );
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+        println!(
+            "Success! Check out your TX here:
+    https://explorer.solana.com/tx/{}/?cluster=devnet",
+            signature
+        );
+    }
 }
