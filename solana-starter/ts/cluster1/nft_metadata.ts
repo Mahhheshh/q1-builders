c3fe9b900ff4f225d1954cf185ccd4d7e4e0fdb8
Mahhheshh <100200105+Mahhheshh@users.noreply.github.com>
2025-01-16 23:43:05 +0530

    add nft metadata

--- solana-starter/ts/cluster1/nft_metadata.ts
+++ solana-starter/ts/cluster1/nft_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -17,27 +17,28 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        // const image = await umi.downloader.download(["DgrGZJpNUsnDggL27GxmUimutyXoUS95QtYKk2jZEtTR"])
+        const image = "https://devnet.irys.xyz/DgrGZJpNUsnDggL27GxmUimutyXoUS95QtYKk2jZEtTR";
+        const metadata = {
+            name: "fartRug",
+            symbol: "FRug",
+            description: "The king of no where farterd in this art",
+            image: image,
+            attributes: [
+                {trait_type: 'Brown', value: '1'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myUri = await umi.uploader.uploadJson(metadata)
+        console.log("Your metadata URI: ", myUri); // DdvfMeZr7ybCTmYLr4VqiWxxY45onhq3bbofJshQ5GF9
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
